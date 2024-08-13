78aa200db845d64d38b77e46c30a5683651fbe50
Beatriz <beatriz.bonatto@fatec.sp.gov.br>
2024-08-13 19:57:25 -0300

    first commit

---
+++ src/main/java/br/gov/sp/fatec/springbotlab04/Springbotlab04Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springbotlab04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbotlab04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbotlab04Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/springbotlab04/Springbotlab04ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springbotlab04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbotlab04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
