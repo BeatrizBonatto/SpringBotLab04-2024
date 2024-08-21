46377f97705dc892613c9cbd6cf0c7339cdb8911
Beatriz Bonatto <beatriz.bonatto@fatec.sp.gov.br>
2024-08-21 00:18:49 +0000

    Tirando comentario

--- src/main/java/gov/sp/fatec/springbotlab04/controller/UsuarioController.java
+++ src/main/java/gov/sp/fatec/springbotlab04/controller/UsuarioController.java
@@ -1,10 +1,17 @@
 package gov.sp.fatec.springbotlab04.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import gov.sp.fatec.springbotlab04.entity.Usuario;
 import gov.sp.fatec.springbotlab04.service.UsuarioService;
 
 @RestController
@@ -15,18 +22,18 @@ public class UsuarioController {
     @Autowired
     private UsuarioService service; //sempre declare com interface, nunca com usuário
     
-    // @GetMapping
-    // public List<Usuario> todosUsuarios() {
-    //     return service.todosUsuarios();
-    // }
+    @GetMapping
+    public List<Usuario> todosUsuarios() {
+        return service.todosUsuarios();
+    }
 
-    // @PostMapping
-    // public Usuario novUsuario(@RequestBody Usuario usuario) {
-    //     return service.novoUsuario(usuario);
-    // }
+    @PostMapping
+    public Usuario novUsuario(@RequestBody Usuario usuario) {
+        return service.novoUsuario(usuario);
+    }
     
-    // @GetMapping(value = "/{id}") //diferenciar do outro get
-    // public Usuario buscarPeloId(@PathVariable("id") Long id) {
-    //     return service.buscarPeloId(id);
-    // }
+    @GetMapping(value = "/{id}") //diferenciar do outro get
+    public Usuario buscarPeloId(@PathVariable("id") Long id) {
+        return service.buscarPeloId(id);
+    }
 }
